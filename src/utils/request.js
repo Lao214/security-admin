dc531d8901e020a6bf14d281caf6174658e5abd9
Lao214 <112752790+Lao214@users.noreply.github.com>
2022-11-30 08:49:09 +0800

    前端整合权限

--- src/utils/request.js
+++ src/utils/request.js
@@ -16,10 +16,7 @@ service.interceptors.request.use(
     // do something before request is sent
 
     if (store.getters.token) {
-      // let each request carry token
-      // ['X-Token'] is a custom headers key
-      // please modify it according to the actual situation
-      config.headers['X-Token'] = getToken()
+      config.headers['token'] = getToken()
     }
     return config
   },
@@ -32,16 +29,6 @@ service.interceptors.request.use(
 
 // response 拦截器
 service.interceptors.response.use(
-  /**
-   * If you want to get http information such as headers or status
-   * Please return  response => response
-  */
-
-  /**
-   * Determine the request status by custom code
-   * Here is just an example
-   * You can also judge the status by HTTP Status Code
-   */
   response => {
     const res = response.data
 
--- src/views/system/sysRole/list.vue
+++ src/views/system/sysRole/list.vue
@@ -19,7 +19,7 @@
     </div>
     <!-- 工具条 -->
     <div class="tools-div">
-      <el-button type="success" icon="el-icon-plus" size="mini" @click="add">添 加</el-button>
+      <el-button type="success" :disabled="$hasBP(btn.sysRole.add) === false" icon="el-icon-plus" size="mini" @click="add">添 加</el-button>
       <el-button class="btn-add" size="mini" @click="batchRemove()" >批量删除</el-button>
     </div>
     <!-- 表格 -->
